c63d35c6b2932f3d35e901c414bcf5446c53fb2f
alaabashakim <alaabashakim@gmail.com>
2022-02-18 21:35:01 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,22 @@
+fun main(){
+  Name()
+    add(5,2)
+    sum(7,4,2,1)
+}
+fun Name(){
+    var name ="Allas"
+    println("Hello " +name)
+}
+fun add(a: Int, b: Int){
+    var result=a%b
+    println(result)
+
+
+}
+fun sum(a: Int, b: Int, c: Int, d: Int){
+    var result=a+b+c+d
+    println(result)
+}
+fun interesting(){
+    println("iam enable to develop some coding steps espically in khotline class")
+}
